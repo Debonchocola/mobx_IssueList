e784df9d7e6cc1f7bf576d12352074d5bf7d08bf
ubpark <pub10@naver.com>
2023-02-07 10:22:07 +0900

    Feat: pages 경로 설정

--- mobx-issuelist/src/App.js
+++ mobx-issuelist/src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Detail, Main } from "./pages";
+
 function App() {
-  return <div>1</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/:number" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
